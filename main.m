a8de02a3d642873b22260e5a67e5fbc44b9d8dd5
Gabriel Brunoro <92934338+Brun0r0@users.noreply.github.com>
2025-07-16 21:00:57 -0300

    Update main.m

--- main.m
+++ main.m
@@ -1,24 +1,53 @@
-% Autores:
-    % Gabriel C. Brunoro
-    % Pedro F. Sabino
+% ------------------------------------------------------------
+% Sistema de Correção Automática de Provas OMR
+% 
+% Este script realiza a leitura e análise de folhas de resposta
+% de provas do tipo múltipla escolha utilizando técnicas de 
+% reconhecimento óptico de marcas (OMR). Através do processamento 
+% de imagem, identifica as alternativas marcadas, compara com o 
+% gabarito e gera as notas automaticamente.
+% ------------------------------------------------------------
+% Como utilizar:
+%   1º Adicionar um arquivo gabarito no formato .txt como está no
+%        arquivo gabarito_exemplo.txt
+%   2º Adicionar as fotos das folhas respostas na pasta Folhas_Respostas
+%   3º Rodar o arquivo main.m e responder qual é o gabarito que deseja
+%       utilizar para a correção
+%   4º Os resultados estarão armazenados na pasta Notas
+% ------------------------------------------------------------
+% Autor:
+%   Gabriel C. Brunoro
 
 %Limpeza
 clear, clc, close all;
 
-%Colocar o arquivo gabarito na pasta /templates/
-gabaritoName = input("Enter the name of the template: ", 's');
+%Colocar o arquivo gabarito na pasta /gabaritos/
+gabaritoName = input("Digite o nome do gabarito: ", 's');
 
-caminho = 'templates/' + string(gabaritoName) + '.txt';
+caminho = 'gabaritos/' + string(gabaritoName) + '.txt';
 
 % Lê o gabarito e aloca na array gabarito
 gabarito = ler_gabarito(caminho);
 
-% Leitura das folhas respostas na pasta
-caminho = "Answer sheets/";
-files = dir(caminho + "*.j*eg");
+% Adicionar todas folhas respostas na pasta correspondente
+% Leitura das folhas respostas
 
+
+% Lista de extensões suportadas
+extensoes = ["*.jpg", "*.jpeg", "*.png", "*.bmp", "*.tif", "*.tiff"];
+
+caminho = "Folhas_Respostas/";
+files = [];
+
+% Buscar arquivos com cada extensão
+for i = 1:length(extensoes)
+    files = [files; dir(fullfile(caminho, extensoes(i)))];
+end
+
+% Realiza a contagem de arquivos da pasta
 qntFolhas = length(files);
 
+% Inicialização de arrays
 nomes = {};
 notas = [];
 
@@ -33,12 +62,21 @@ for k = 1:qntFolhas
         [~, baseName, ~] = fileparts(arqName);
 
         imagem = imread(caminho + arqName);
+
+        % Descomentar para ver imagem de entrada
+        %figure, imshow(imagem);
         
         % Correção de inclinação
         imagem_rotacionada = corrigir_inclinacao(imagem);
         
+        % Descomentar para ver imagem rotacionada
+        %figure, imshow(imagem_rotacionada);
+        
         % Secciona figura principal
         imagem_MO = recortar_MOs(imagem_rotacionada);
+
+        % Descomentar para ver imagem recortada
+        %figure, imshow(imagem_MO);
         
         % Identificação das respostas
         respostas = identificar_respostas(imagem_MO);
@@ -57,6 +95,6 @@ for k = 1:qntFolhas
 end
 
 % Salvando planilha "Notes"
-writetable(planilhaNotas, "Notes/All.xls");
+writetable(planilhaNotas, "Notes/all.xls");
 
 disp(planilhaNotas);
